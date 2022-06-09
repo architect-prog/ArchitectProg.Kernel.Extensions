6b894962fb8d6922e86e191d2c969e784e06079e
architect-prog <maxim.lushh@mail.ru>
2022-06-09 02:28:53 +0200

    add base specification implementation

---
+++ ArchitectProg.Kernel.Extensions/Base/Specification.cs
@@ -0,0 +1,36 @@
+﻿using ArchitectProg.Kernel.Extensions.Interfaces;
+
+namespace ArchitectProg.Kernel.Extensions.Base;
+
+public abstract class Specification<T> : ISpecification<T>
+{
+    public virtual IQueryable<T> AddEagerFetching(IQueryable<T> query)
+    {
+        if (query == null)
+        {
+            throw new ArgumentNullException(nameof(query));
+        }
+
+        return query;
+    }
+
+    public virtual IQueryable<T> AddPredicates(IQueryable<T> query)
+    {
+        if (query == null)
+        {
+            throw new ArgumentNullException(nameof(query));
+        }
+
+        return query;
+    }
+
+    public virtual IQueryable<T> AddSorting(IQueryable<T> query)
+    {
+        if (query == null)
+        {
+            throw new ArgumentNullException(nameof(query));
+        }
+
+        return query;
+    }
+}
