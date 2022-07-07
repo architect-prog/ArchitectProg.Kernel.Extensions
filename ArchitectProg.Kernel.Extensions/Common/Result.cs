4804fc89b74967a90421cc5acb555637a93db519
architect-prog <maxim.lushh@mail.ru>
2022-07-07 20:20:09 +0200

    add result type for dto wrapping

---
+++ ArchitectProg.Kernel.Extensions/Common/Result.cs
@@ -0,0 +1,33 @@
+﻿namespace ArchitectProg.Kernel.Extensions.Common;
+
+public class Result<T>
+{
+    private readonly T? value;
+    private readonly Exception? exception;
+
+    public T? ValueOrDefault => value;
+    public bool IsSuccess => exception is null;
+
+    public Result(T value)
+    {
+        this.value = value;
+    }
+
+    public Result(Exception exception)
+    {
+        value = default;
+        this.exception = exception;
+    }
+
+    public R Match<R>(Func<T?, R> success, Func<Exception?, R> fail)
+    {
+        var result = IsSuccess ? success(value) : fail(exception);
+        return result;
+    }
+
+    public static implicit operator Result<T>(T source)
+    {
+        var result = new Result<T>(source);
+        return result;
+    }
+}
---
+++ ArchitectProg.Kernel.Extensions/Common/ResultFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace ArchitectProg.Kernel.Extensions.Common;
+
+public static class ResultFactory
+{
+    public static Result<T> Failure<T>(Exception exception)
+    {
+        var result = new Result<T>(exception);
+        return result;
+    }
+}
