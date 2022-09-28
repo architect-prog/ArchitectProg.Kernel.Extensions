62ea53d2541f276d25a5f3d9278118ed112e2bd7
architect-prog <maxim.lushh@mail.ru>
2022-09-28 19:59:56 +0200

    add cancelation token to repository

--- ArchitectProg.Kernel.Extensions/Interfaces/IRepository.cs
+++ ArchitectProg.Kernel.Extensions/Interfaces/IRepository.cs
@@ -2,14 +2,18 @@
 
 public interface IRepository<T> where T : class
 {
-    Task Add(T entity);
-    Task AddRange(IEnumerable<T> entities);
-    Task Update(T entity);
-    Task UpdateRange(IEnumerable<T> entities);
-    Task Delete(T entity);
-    Task<int> Count(ISpecification<T> specification);
-    Task<bool> Exists(ISpecification<T> specification);
-    Task<T?> GetOrDefault(object id);
-    Task<T?> GetOrDefault(ISpecification<T> specification);
-    Task<T[]> List(ISpecification<T> specification, int? skip = null, int? take = null);
+    Task Add(T entity, CancellationToken token = default);
+    Task AddRange(IEnumerable<T> entities, CancellationToken token = default);
+    Task Update(T entity, CancellationToken token = default);
+    Task UpdateRange(IEnumerable<T> entities, CancellationToken token = default);
+    Task Delete(T entity, CancellationToken token = default);
+    Task<int> Count(ISpecification<T> specification, CancellationToken token = default);
+    Task<bool> Exists(ISpecification<T> specification, CancellationToken token = default);
+    Task<T?> GetOrDefault(object id, CancellationToken token = default);
+    Task<T?> GetOrDefault(ISpecification<T> specification, CancellationToken token = default);
+    Task<T[]> List(
+        ISpecification<T> specification,
+        int? skip = null,
+        int? take = null,
+        CancellationToken token = default);
 }
